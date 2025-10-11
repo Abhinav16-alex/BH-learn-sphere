a821822f01dc26db431cb6846e14f1540a373301
Peddireddy Shanmukha Abhinavi Pradeep <shanmukha.1616@gmail.com>
2025-10-11 14:52:50 +0530

    Create apps.py

---
+++ apps/collaboration/apps.py
@@ -0,0 +1,8 @@
+# Save as: apps/collaboration/apps.py
+
+from django.apps import AppConfig
+
+class CollaborationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.collaboration'
+    label = 'collaboration'
