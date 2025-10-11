3b4a84acb2ba3fbe5aeccf0f3768c366f4e76d21
Peddireddy Shanmukha Abhinavi Pradeep <shanmukha.1616@gmail.com>
2025-10-11 14:49:09 +0530

    Create apps.py

---
+++ apps/courses/apps.py
@@ -0,0 +1,8 @@
+# Save as: apps/courses/apps.py
+
+from django.apps import AppConfig
+
+class CoursesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.courses'
+    label = 'courses'
